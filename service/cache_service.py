6da6eb1e748bc1636b93bef4074fd36707bb1480
hejp009 <hejunpeng2012@hotmail.com>
2020-06-12 16:01:39 +0800

    get standard word locally

--- service/cache_service.py
+++ service/cache_service.py
@@ -14,14 +14,14 @@ enable_cache = False
 def store_keyword_df_cache(df: pd.DataFrame):
     if not enable_cache:
         return
-    redis_template.set(KEYWORD_DF_KEY, df.to_msgpack(compress="zlib"))
+    redis_template.db(0).set(KEYWORD_DF_KEY, df.to_msgpack(compress="zlib"))
     log.info("stored the keyword_df in redis as cache")
 
 
 def get_keyword_df_cache() -> Optional[pd.DataFrame]:
     if not enable_cache:
         return
-    cache = redis_template.get(KEYWORD_DF_KEY)
+    cache = redis_template.db(0).get(KEYWORD_DF_KEY)
     if cache:
         log.info("found the keyword_df cache in redis")
         return pd.read_msgpack(cache)
@@ -30,14 +30,30 @@ def get_keyword_df_cache() -> Optional[pd.DataFrame]:
 def store_matrix_cache(matrix: np.ndarray):
     if not enable_cache:
         return
-    redis_template.set(MATRIX_KEY, json.dumps(matrix.tolist()))
+    redis_template.db(0).set(MATRIX_KEY, json.dumps(matrix.tolist()))
     log.info("stored the matrix in redis as cache")
 
 
 def get_matrix_cache() -> Optional[np.ndarray]:
     if not enable_cache:
         return
-    cache = redis_template.get(MATRIX_KEY)
+    cache = redis_template.db(0).get(MATRIX_KEY)
     if cache:
         log.info("found matrix cache in redis")
         return np.asarray(json.loads(cache), dtype=np.float64)
+
+
+def get_standard_word_cache(other_word: str) -> str:
+    cache = redis_template.db(3).get(other_word)
+    if cache:
+        return cache.decode("utf-8")
+    else:
+        return other_word
+
+
+def get_standard_category_cache(standard_word: str) -> Optional[str]:
+    cache = redis_template.db(4).get(standard_word)
+    if cache:
+        return cache.decode("utf-8")
+    else:
+        return "None"
